444bb978f37bf9eb6e5ad53a027a2696a9a7911a
hudji <shudjimartsu@gmail.com>
2019-02-20 16:33:04 +0700

    testing

--- 21122018/21122018RUN-SH.py
+++ 21122018/21122018RUN-SH.py
@@ -10,12 +10,12 @@ from Modul_Topo.FORESTS2020 import allFunc
 from Modul_ML.F17122018ML import F2020ML
 
 # df = pd.read_csv(r'D:\FORESTS2020\GITHUB\Plugin\GitTesis\SVR\DBSCAN_5_I.csv')
-# df = pd.read_csv(r'D:\FORESTS2020\GITHUB\Plugin\GitTesis\SVR\Dbscan1000Samp_0.05_5.csv')
-script = sys.argv[0]
-input = sys.argv[1]
+df = pd.read_csv(r'D:\FORESTS2020\TRAINING\R\SVR\Dbscan1000Samp_1.5_5.csv')
+# script = sys.argv[0]
+# input = sys.argv[1]
 df = pd.read_csv(input)
 # print(df.head())
-column = ['Band_2', 'Band_3', 'Band_4', 'Band_5', 'Band_6', 'Band_7']
+column = ['123064_DEM','ASPECT_R','SLOPE_R','Band_2', 'Band_3', 'Band_4', 'Band_5', 'Band_6', 'Band_7']
 # column = ['Band_4']
 target = 'frci'
 dfX = pd.DataFrame(df, columns=column)
